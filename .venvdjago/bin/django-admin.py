962ad32553f88b7ff4df963d1335467f75235f35
Elton <elton@localhost.localdomain>
2017-03-02 17:04:44 -0300

    gitignore

--- .venvdjago/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/elton/Documentos/Projetos/Python/WEB/Django/.venvdjago/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
